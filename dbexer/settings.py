8d3649eaf86e5309c4da7bd4526e3f5b3f4ccb5d
codewitgabi <gabrielmicheal497@gmail.com>
2023-01-12 19:32:50 +0100

    Changed db password

--- dbexer/settings.py
+++ dbexer/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': "railway",
         "USER": "postgres",
-        "PASSWORD": "HnTwKMCU9RCi1e0402P0",
+        "PASSWORD": "nSpTrHUOchDc8kJEpvbE",
         "HOST": "containers-us-west-144.railway.app",
         "PORT": "6849"
     }
